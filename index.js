8383a000509ee33fe428c4818678aa38885e80b1
Kimani <elvis.kimani@student.moringaschool.com>
2022-06-19 15:05:02 +0300

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const passedNumbers = new Set(); // initialize empty array
+  for (const number of array) {
+    const complement = target - number;
+
+    // returns true if the Set includes the complement
+    if (passedNumbers.has(complement)) return true;
+
+    // adds the number to the Set
+    passedNumbers.add(number);
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n)-linear
 */
 
 /* 
   Add your pseudocode here
+    1. Initialize an empty Set
+    2. Loop through the array
+    3. Get the complement of the current number
+    4. Check if the Set includes the complement
+    5. If it does, return true
+    6. If it doesn't, add the number to the Set
+    7. Return false
 */
 
 /*
   Add written explanation of your solution here
+  Loop through the array and check if the complement of the current number is in the Set. If it is, return true. If it isn't, add the number to the Set.
 */
 
 // You can run `node index.js` to view these console logs
